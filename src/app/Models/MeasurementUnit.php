b1610f416580b7acafd95278baa8d40c25d435d2
Mihai <mihai.ocneanu@gmail.com>
2019-11-25 14:35:19 +0200

    added Rememberable trait

--- src/app/Models/MeasurementUnit.php
+++ src/app/Models/MeasurementUnit.php
@@ -3,11 +3,12 @@
 namespace LaravelEnso\MeasurementUnits\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\Rememberable\app\Traits\Rememberable;
 use LaravelEnso\Tables\app\Traits\TableCache;
 
 class MeasurementUnit extends Model
 {
-    use TableCache;
+    use Rememberable, TableCache;
 
     protected $fillable = ['name', 'description'];
 }
