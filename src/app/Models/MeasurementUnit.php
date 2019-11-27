5e682e822fd8175724c11c6e6169a166200ddce9
mishu <mishu@xtelecom.ro>
2019-11-27 13:18:21 +0200

    adds AvoidsDeletionConflicts on measurement units

--- src/app/Models/MeasurementUnit.php
+++ src/app/Models/MeasurementUnit.php
@@ -3,12 +3,13 @@
 namespace LaravelEnso\MeasurementUnits\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\Helpers\app\Traits\AvoidsDeletionConflicts;
 use LaravelEnso\Rememberable\app\Traits\Rememberable;
 use LaravelEnso\Tables\app\Traits\TableCache;
 
 class MeasurementUnit extends Model
 {
-    use Rememberable, TableCache;
+    use AvoidsDeletionConflicts, Rememberable, TableCache;
 
     protected $fillable = ['name', 'description'];
 }
