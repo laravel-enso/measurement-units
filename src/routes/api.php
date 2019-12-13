f1857c31ff10878c20353850dfeb8318f0654e91
Mihai <mihai.ocneanu@gmail.com>
2019-12-13 17:51:00 +0200

    split routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,18 +4,6 @@ Route::middleware(['web', 'auth', 'core'])
         Route::namespace('LaravelEnso\MeasurementUnits\app\Http\Controllers')
             ->prefix('api/administration/measurementUnits')->as('administration.measurementUnits.')
             ->group(function () {
-                Route::get('create', 'Create')->name('create');
-                Route::post('', 'Store')->name('store');
-                Route::get('{measurementUnit}/edit', 'Edit')->name('edit');
-
-                Route::patch('{measurementUnit}', 'Update')->name('update');
-
-                Route::delete('{measurementUnit}', 'Destroy')->name('destroy');
-
-                Route::get('initTable', 'InitTable')->name('initTable');
-                Route::get('tableData', 'TableData')->name('tableData');
-                Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
-
-                Route::get('options', 'Options')->name('options');
+                require 'app/measurementUnits.php';
             });
     });
--- src/routes/app/measurementUnits.php
+++ src/routes/app/measurementUnits.php
@@ -1,1 +1,15 @@
 <?php
+
+Route::get('create', 'Create')->name('create');
+Route::post('', 'Store')->name('store');
+Route::get('{measurementUnit}/edit', 'Edit')->name('edit');
+
+Route::patch('{measurementUnit}', 'Update')->name('update');
+
+Route::delete('{measurementUnit}', 'Destroy')->name('destroy');
+
+Route::get('initTable', 'InitTable')->name('initTable');
+Route::get('tableData', 'TableData')->name('tableData');
+Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+
+Route::get('options', 'Options')->name('options');
