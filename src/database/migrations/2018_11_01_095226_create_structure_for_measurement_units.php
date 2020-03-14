dfa7e1ab1129d9a203584720ccdeb1db6883dbd5
mishu <mishu@xtelecom.ro>
2020-03-14 18:32:31 +0200

    removes Types from migration

--- src/database/migrations/2018_11_01_095226_create_structure_for_measurement_units.php
+++ src/database/migrations/2018_11_01_095226_create_structure_for_measurement_units.php
@@ -1,25 +1,24 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForMeasurementUnits extends Migration
 {
     protected $permissions = [
-        ['name' => 'administration.measurementUnits.index', 'description' => 'Show index for measurement unit', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.create', 'description' => 'Create measurement unit', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.store', 'description' => 'Store a new measurement unit', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.edit', 'description' => 'Edit measurement unit', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.update', 'description' => 'Update measurement unit', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.destroy', 'description' => 'Delete measurement unit', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.initTable', 'description' => 'Init table for measurement unit', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.tableData', 'description' => 'Get table data for measurement unit', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.exportExcel', 'description' => 'Export excel for measurement unit', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.measurementUnits.options', 'description' => 'Get measurement unit options for select', 'type' => Types::Read, 'is_default' => false],
+        ['name' => 'administration.measurementUnits.index', 'description' => 'Show index for measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.create', 'description' => 'Create measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.store', 'description' => 'Store a new measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.edit', 'description' => 'Edit measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.update', 'description' => 'Update measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.destroy', 'description' => 'Delete measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.initTable', 'description' => 'Init table for measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.tableData', 'description' => 'Get table data for measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.exportExcel', 'description' => 'Export excel for measurement unit', 'is_default' => false],
+        ['name' => 'administration.measurementUnits.options', 'description' => 'Get measurement unit options for select', 'is_default' => false],
     ];
 
     protected $menu = [
-        'name' => 'Measurement Units', 'icon' => 'balance-scale', 'route' => 'administration.measurementUnits.index', 'order_index' => 450, 'has_children' => false
+        'name' => 'Measurement Units', 'icon' => 'balance-scale', 'route' => 'administration.measurementUnits.index', 'order_index' => 450, 'has_children' => false,
     ];
 
     protected $parentMenu = 'Administration';
