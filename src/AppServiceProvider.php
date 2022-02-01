09b396c019a3a77c9a07a4d8b142cb7bc01169e5
GITmanuela <44998004+GITmanuela@users.noreply.github.com>
2022-02-01 11:39:27 +0200

    fixes seeders path

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
         ], ['measurement-unit-factories', 'enso-factories']);
 
         $this->publishes([
-            __DIR__.'/../database/seeds' => database_path('seeds'),
+            __DIR__.'/../database/seeders' => database_path('seeders'),
         ], ['measurement-unit-seeder',  'enso-seeders']);
     }
 }
