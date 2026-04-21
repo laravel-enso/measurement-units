eeeefca2998a2aeb5a4adebbe5f6879e94504bb2
mishu <mishu@xtelecom.ro>
2026-04-21 18:26:15 +0300

    Apply local changes

--- tests/features/MeasurementUnitsTest.php
+++ tests/features/MeasurementUnitsTest.php
@@ -1,14 +1,17 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use LaravelEnso\DataImport\Models\Import;
 use LaravelEnso\Forms\TestTraits\CreateForm;
 use LaravelEnso\Forms\TestTraits\DestroyForm;
 use LaravelEnso\Forms\TestTraits\EditForm;
+use LaravelEnso\Helpers\Services\Obj;
+use LaravelEnso\MeasurementUnits\Imports\Importers\MeasurementUnits as Importer;
 use LaravelEnso\MeasurementUnits\Models\MeasurementUnit;
 use LaravelEnso\Tables\Traits\Tests\Datatable;
 use LaravelEnso\Users\Models\User;
-use Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
 
 class MeasurementUnitsTest extends TestCase
 {
@@ -74,5 +77,41 @@ class MeasurementUnitsTest extends TestCase
         ], false))
             ->assertStatus(200)
             ->assertJsonFragment(['name' => $this->testModel->name]);
+    }
+
+    #[Test]
+    public function validates_required_name_on_store()
+    {
+        $this->post(route('administration.measurementUnits.store', [], false), [
+            'name' => null,
+            'description' => $this->testModel->description,
+        ])->assertStatus(302)
+            ->assertSessionHasErrors(['name']);
+    }
+
+    #[Test]
+    public function validates_unique_name_on_store()
+    {
+        $this->testModel->save();
+
+        $this->post(route('administration.measurementUnits.store', [], false), [
+            'name' => $this->testModel->name,
+            'description' => 'Duplicate',
+        ])->assertStatus(302)
+            ->assertSessionHasErrors(['name']);
+    }
+
+    #[Test]
+    public function can_import_measurement_unit()
+    {
+        (new Importer())->run(new Obj([
+            'name' => 'Imported Unit',
+            'description' => 'Imported description',
+        ]), \Mockery::mock(Import::class));
+
+        $this->assertDatabaseHas('measurement_units', [
+            'name' => 'Imported Unit',
+            'description' => 'Imported description',
+        ]);
     }
 }
